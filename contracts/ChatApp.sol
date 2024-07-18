588c64701a6e6a4683c147c6a16e11838fe11df8
Pyrokryos <dulina.perera@outlook.com>
2024-07-18 20:29:01 +0530

    Defined the necessary structs and defined functions to check if a user exist and to create a new account.

--- contracts/ChatApp.sol
+++ contracts/ChatApp.sol
@@ -2,5 +2,39 @@
 pragma solidity ^0.8.19;
 
 contract ChatApp {
-    
+    // User struct
+    struct User {
+        string name;
+        Friend[] friends;
+    }
+
+    // Friend struct
+    struct Friend {
+        address publicKey;
+        string name;
+    }
+
+    // Message struct
+    struct Message {
+        address from;
+        address to;
+        uint256 timestamp;
+        string content;
+    }
+
+    mapping(address => User) users; // Mapping of public key to User
+    mapping(bytes32 => Message[]) messages; // Mapping of hash to Message
+
+    // Check if user exists.
+    function doesUserExist(address publicKey) public view returns (bool) {
+        return bytes(users[publicKey].name).length > 0;
+    }
+
+    // Create a new account.
+    function createAccount(string calldata name) external {
+        require(!doesUserExist(msg.sender), "User already exists!");
+        require(bytes(name).length > 0, "Name cannot be empty!");
+
+        users[msg.sender].name = name;
+    }
 }
