ac441e4f5ad89b8e2204956a5e7a6a28e16b9fba
Pyrokryos <dulina.perera@outlook.com>
2024-07-19 12:50:22 +0530

    Defined two functions, one to check if two users are friends and the other to add a friend.

--- contracts/ChatApp.sol
+++ contracts/ChatApp.sol
@@ -39,9 +39,42 @@ contract ChatApp {
     }
 
     // Find user name given user address.
-    function getUserName(address publicKey) public returns (string memory) {
+    function getUserName(address publicKey) external returns (string memory) {
         require(doesUserExist(publicKey), "User does not exist!");
 
         return users[publicKey].name
+    }
+
+    // Check whether two users are friends.
+    function areFriends(address publicKey1, address publicKey2) internal view returns (bool) {
+        if (users[publicKey1].friends.length > users[publicKey2].friends.length) {
+            address temporary = publicKey1;
+            publicKey1 = publicKey2;
+            publicKey2 = temporary;
+        }
+
+        for (uint256 i = 0; i < users[publicKey1].friends.length; i++) {
+            if (users[publicKey1].friends[i].publicKey == publicKey2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Add a friend. (Internal)
+    function _addFriend(address publicKey1, address publicKey2, string memory name) internal {
+        Friend memory friend = Friend(publicKey2, name);
+        users[publicKey1].friends.push(friend);
+    }
+
+    // Add a friend. (Extenal)
+    function addFriend(address publicKey, string calldata name) external {
+        require(doesUserExist(msg.sender), "Create an account first!");
+        require(doesUserExist(publicKey), "The user you're trying to add does not exist!");
+        require(msg.sender != publicKey, "You can't add yourself as a friend.");
+        require(!areFriends(msg.sender, publicKey), "You two are already friends!");
+
+        _addFriend(msg.sender, publicKey, name);
+        _addFriend(publicKey, msg.sender, users[msg.sender].name);
     }
 }
